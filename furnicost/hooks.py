780a331f5002c2ba0affec57796944cbcbeb35bc
Ghostridero0o <crazycrazy9x@gmail.comm>
2025-08-21 08:15:56 +0700

    commit

--- furnicost/hooks.py
+++ furnicost/hooks.py
@@ -241,4 +241,13 @@ app_license = "mit"
 # default_log_clearing_doctypes = {
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
-
+fixtures = [
+    {
+        "dt": "Custom Field",
+        "filters": [["module", "=", "Furnicost"]]
+    },
+    {
+        "dt": "Property Setter",
+        "filters": [["module", "=", "Furnicost"]]
+    }
+]
